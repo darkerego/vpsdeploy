5f19746975209f16c9ac3c4c443fe8604e0a1a64
darkerego <donotaccept@gmx.com>
2015-02-22 20:33:27 -0500

    Update deploy.sh
    Fixed apt syntax, split into more functions.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #######################################
 ## VPS Deploy Version  1.1 |------>  ##
-## 	    Author Chev Y. |	           ##
+## 	    Author Chev Y. |	     ##
 #######################################
 #!/bin/bash
 #
@@ -19,9 +19,11 @@ SSHERROR1='SSH Configuration Error'
 SSHERROR2='ERROR SETTING PUBKEY'
 SSHERROR3='ERROR Restarting ssh daemon!'
 FWERROR1='Error enabling firewall!'
-FWERROR2='Error opening port!'
+FWERROR2='Error opening port or port already open.'
 AptError1='Error updating system!'
+userERROR="Cannot cd to /home/$username"
 #######################################
+RIGHT_NOW=$(date +"%x %r %Z")
 
 function disp_HELP()
 {
@@ -42,7 +44,7 @@ echo -e "
 
 function config_USER()
 {
-	touch $DEPLOG && echo date >> $DEPLOG
+	touch $DEPLOG && echo $RIGHT_NOW >> $DEPLOG
 	echo "Specify credentials." 	#Username+Password of admin user
 
 	read -p "Enter username : " username
@@ -62,26 +64,26 @@ function config_SSH()
 
 cd /etc/ssh || echo $SSHERROR1
 mv sshd_config sshd_config.orig || echo $SSHERROR1 	  # Backup sshd original
-cp -p $CONFDIR/sshd_config sshd_config || echo $SSHERROR1 # Preserve permissions&
+cp -p $CONFDIR/sshd_config /etc/ssh/sshd_config || echo $SSHERROR1 # Preserve permissions&
 						  	#copy sshd_config
 						  	# Copies pub key to admin's ~/.ssh:
-cd /home/$username
+cd /home/$username || echo $userERROR
 
 if [ ! -d /home/$username/.ssh ]; then
-    mkdir .ssh && cp -p $CONFDIR/authorized_keys .ssh/authorized_keys || echo $SSHERROR
+    mkdir .ssh && cp -p $CONFDIR/authorized_keys /home/$username/.ssh/authorized_keys || echo $SSHERROR1
 else
-   cp -p $CONFDIR/authorized_keys .ssh/authorized_keys || echo $SSHERROR2
+   cp -p $CONFDIR/authorized_keys /home/$username/.ssh/authorized_keys || echo $SSHERROR2
 fi
 
-chmod 600 .ssh/authorized_keys 				# Ensures integrity of permissions
-cd .. && chmod 700 .ssh
+chmod 600 /home/$username/.ssh/authorized_keys 				# Ensures integrity of permissions
+chmod 700 /home/$username/.ssh
 chown -R $username:$username /home/$username		# Make sure all config files are owned by user
 }
 
 function config_FW()
 {
 
-apt-get install ufw -qq > /dev/null			# Ensure ufw is installed
+apt-get install ufw -y -qq > /dev/null			# Ensure ufw is installed
 echo "Specify port number for incoming ssh connections" #port for ufw to open for ssh
 read SHPORT
 ufw allow $SHPORT >> $DEPLOG || echo $FWERROR2 >> $DEPLOG			# Open configured ssh port
@@ -95,26 +97,33 @@ ufw enable >> $DEPLOG || echo $FWERROR1 >> $DEPLOG
 service ssh restart >> $DEPLOG || echo $SSHERROR3 >> $DEPLOG 			# Reload our new configuration
 
 echo Done. Server is secure. Now updating software...Please try to login as $username on port $SHPORT with your private key.
+}
 
+function update_SYS()
+{
+	echo "Performing System Updates..." # Update repos&software
 
+apt-get update -qq && apt-get upgrade -y -qq >> $DEPLOG || echo $AptError1 >> $DEPLOG
 
-echo "Performing System Updates..." # Update repos&software
-	apt-get update -qq && apt-get upgrade -qq >> /dev/null || echo $AptError1 >> $DEPLOG
-echo "Done. Installing software from list..."
+	echo "Now installing: $GETLIST..."
 
-apt-get install $GETLIST -qq || echo "Error installing some  program(s)" >> $DEPLOG # Install/Remove desired programs
-apt-get remove $KILLLIST -qq || echo "Kill list error!" >> $DEPLOG ;
+apt-get install -y -qq $GETLIST || echo "Error installing some  program(s)" >> $DEPLOG # Install/Remove desired programs
+	echo "Removing programs: $KILLLIST"
+apt-get remove -y -qq $KILLLIST || echo "Kill list error!" >> $DEPLOG ;
 }
 
 function dont_KILL()
 {
-ufw disable	# In case something goes wrong and you get locked out
-sleep 120	# Disable UFW for a 2 min window so you can log in,
-ufw enable	# then reenable it. (Should not happen)
+ufw disable >> $DEPLOG	# In case something goes wrong and you get locked out
+sleep 120		# Disable UFW for a 2 min window so you can log in,
+ufw enable >> $DEPLOG		# then reenable it. (Should not happen)
 }
 
 disp_HELP
 config_USER
 config_SSH
 config_FW
+update_SYS
 dont_KILL
+
+exit
